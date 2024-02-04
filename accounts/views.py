c011b0fafdc575f44e77998c43049eebff67223d
dinukah99 <dinukahamarasinghe@gmail.com>
2024-02-04 20:48:54 +0530

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from .serializers import UserRegisterSerializer
+from rest_framework.response import Response
+
+
+class RegisterUserView(GenericAPIView):
+    serializer_class = UserRegisterSerializer
+
+    def post(self, request):
+        user_data = request.data
+        serializer = self.serializer_class(data=user_data)
+        if serializer.is_valid():
+            serializer.save()
+            user = serializer.data
+            # send email function user['email']
+            return Response({
+                'data': user,
+                'message': f'Hi {user.first_name} thanks for signing up! Passcode has been sent to your email. Please '
+                           f'verify your email to complete the registration.'
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
